8b1042b64db5500e22d4c4057d9f39738ade00d3
bcecilpgh <bc@freshtech.me>
2026-03-14 20:53:13 -0400

    Update search.js

--- functions/jetbuilt/search.js
+++ functions/jetbuilt/search.js
@@ -1,18 +1,32 @@
 export async function onRequestGet(context) {
+  const url = new URL(context.request.url);
+  const query = url.searchParams.get('query') || '';
+  const page = url.searchParams.get('page') || '1';
+  const upstream = 'https://api.jetbuilt.com/api/projects?query=' + encodeURIComponent(query) + '&page=' + page + '&active=true';
+
+  const info = { upstream: upstream, hasKey: !!context.env.JETBUILT_API_KEY };
+
   try {
-    const res = await fetch('https://httpbin.org/get');
+    const res = await fetch(upstream, {
+      headers: {
+        'Authorization': 'Token token=' + context.env.JETBUILT_API_KEY,
+        'Accept': 'application/vnd.jetbuilt.v1'
+      }
+    });
+    info.status = res.status;
+    info.statusText = res.statusText;
     const data = await res.text();
-    return new Response(data, {
+    info.bodyLength = data.length;
+    info.bodyPreview = data.substring(0, 500);
+    return new Response(JSON.stringify(info), {
       status: 200,
       headers: { 'Content-Type': 'application/json' }
     });
   } catch (e) {
-    return new Response(JSON.stringify({
-      error: true,
-      name: e.name,
-      message: e.message
-    }), {
-      status: 502,
+    info.error = e.message;
+    info.stack = e.stack;
+    return new Response(JSON.stringify(info), {
+      status: 200,
       headers: { 'Content-Type': 'application/json' }
     });
   }
